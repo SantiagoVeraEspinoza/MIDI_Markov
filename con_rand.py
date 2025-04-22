4bcff217c71cc45f467cceb021870f807121fc2a
Santiago Vera Espinoza <santiveraespinoza@gmail.com>
2025-04-22 01:22:11 -0600

    Added rand library

---
+++ con_rand.py
@@ -0,0 +1,27 @@
+import numpy as np
+import math
+from datetime import datetime
+
+a = 25214903917
+c = 11
+m = 2**48 - 1
+
+now = datetime.now()
+X_N = int(now.strftime('%Y%m%d%H%M%S') + f"{now.microsecond:06d}")
+
+min_x = float('inf')
+max_x = float('-inf')
+
+
+def rand(low, high):
+    global X_N
+    x = (a * X_N + c) % m
+
+    X_N = (x - low) / (high - low)
+
+    return (X_N % (high-low)) + low
+
+def rand_normal():
+    global X_N
+
+    return math.trunc((rand(0, 100) / 100) * 100) / 100
--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import midi
+import con_rand as rand
 
-midi.generate(['DO3', 'RE3', 'MI3', 'FA3', 'SOL3', 'LA3', 'SI3', 'DO4', 'RE4', 'MI4', 'FA4', 'SOL4', 'LA4', 'SI4', 'DO5'])
+print(rand.rand_normal())
+print(rand.rand_normal())
+print(rand.rand_normal())
+print(rand.rand_normal())
+
+# midi.generate(['DO3', 'RE3', 'MI3', 'FA3', 'SOL3', 'LA3', 'SI3', 'DO4', 'RE4', 'MI4', 'FA4', 'SOL4', 'LA4', 'SI4', 'DO5'])
