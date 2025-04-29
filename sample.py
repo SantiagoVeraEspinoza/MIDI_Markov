61cb3fda3be0e75ec577a447ce445183e85beae6
Santiago Vera Espinoza <santiveraespinoza@gmail.com>
2025-04-28 19:47:07 -0600

    Added sampling to music generation

--- sample.py
+++ sample.py
@@ -1,6 +1,16 @@
-import midi
+import mido
+import math
+import os
 import con_rand as rand
-import sample
+import random
+
+NOTES = {
+    'do3': 48,  'do#3': 49, 're3': 50,  're#3': 51, 'mi3': 52,
+    'fa3': 53,  'fa#3': 54, 'sol3': 55, 'sol#3': 56, 'la3': 57, 'la#3': 58, 'si3': 59,
+    'do4': 60,  'do#4': 61, 're4': 62,  're#4': 63, 'mi4': 64,
+    'fa4': 65,  'fa#4': 66, 'sol4': 67, 'sol#4': 68, 'la4': 69, 'la#4': 70, 'si4': 71,
+    'do5': 72
+}
 
 adj_list = {
     'DO3': {'DO3': 0, 'DO#3': 0, 'RE3': 0, 'RE#3': 0, 'MI3': 0, 'FA3': 0, 'FA#3': 0, 'SOL3': 0, 'SOL#3': 0, 'LA3': 0, 'LA#3': 0, 'SI3': 0, 'DO4': 0, 'DO#4': 0, 'RE4': 0, 'RE#4': 0, 'MI4': 0, 'FA4': 0, 'FA#4': 0, 'SOL4': 0, 'SOL#4': 0, 'LA4': 0, 'LA#4': 0, 'SI4': 0, 'DO5': 0}, 
@@ -30,51 +40,92 @@ adj_list = {
     'DO5': {'DO3': 0, 'DO#3': 0, 'RE3': 0, 'RE#3': 0, 'MI3': 0, 'FA3': 0, 'FA#3': 0, 'SOL3': 0, 'SOL#3': 0, 'LA3': 0, 'LA#3': 0, 'SI3': 0, 'DO4': 0, 'DO#4': 0, 'RE4': 0, 'RE#4': 0, 'MI4': 0, 'FA4': 0, 'FA#4': 0, 'SOL4': 0, 'SOL#4': 0, 'LA4': 0, 'LA#4': 0, 'SI4': 0, 'DO5': 0}
 }
 
-sample.take_samples('base_weights.txt', 'final_weights.txt')
+def get_midi_notes(path):
+    # Carga el archivo MIDI
+    mid = mido.MidiFile(path)
 
-with open('final_weights.txt', 'r') as file:
-    lines = file.readlines()
+    # Lista para guardar las notas
+    notes = []
 
-    for i, source_note in enumerate(adj_list):
-        weights = lines[i].strip().split()
-        for j, target_note in enumerate(adj_list[source_note]):
-            adj_list[source_note][target_note] = float(weights[j])
+    # Recorremos todos los mensajes en todas las pistas
+    for track in mid.tracks:
+        for msg in track:
+            if msg.type == 'note_on' and msg.velocity > 0:
+                notes.append(msg.note)
 
-    file.close()
+    return notes
 
-def get_initial_note():
-    notes = list(adj_list.keys())
-    step = 1 / len(notes)
-    rand_num = rand.rand_normal()
+def convert_to_notes(note_list):
+    n_note_list = []
+    for note in note_list:
+        n_note_str = random.choice(list(adj_list.keys()))
+        for note_str, note_num in NOTES.items():
+            if note == note_num:
+                n_note_str = note_str
 
-    acum = step
-    n = 0
-    
-    while acum < rand_num:
-        acum += step
-        n += 1
+        n_note_list.append(n_note_str)
 
-    return notes[n]
+    return n_note_list
 
-def get_next_note(curr_note):
-    notes = list(adj_list.keys())
-    tran = [adj_list[curr_note][target_note] for target_note in adj_list[curr_note]]
-    rand_num = rand.rand_normal()
+def preload_transition_matrix(path):
+    with open(path, 'r') as file:
+        lines = file.readlines()
 
-    acum = tran[0]
-    n = 0
+        for i, source_note in enumerate(adj_list):
+            weights = lines[i].strip().split()
+            for j, target_note in enumerate(adj_list[source_note]):
+                adj_list[source_note][target_note] = float(weights[j])
 
-    while acum < rand_num:
-        n += 1
-        acum += tran[n]
+        file.close()
 
-    return notes[n]
+def save_transition_matrix(path):
+    with open(path, 'w') as file:
+        for source_note in adj_list:
+            line = ''
+            for target_note in adj_list[source_note]:
+                adj_list[source_note][target_note] = math.trunc(adj_list[source_note][target_note] * 10000) / 10000
+                line += f"{adj_list[source_note][target_note]} "
+            line += "\n"
 
-CURR_NOTE = get_initial_note()
-MELODY = [CURR_NOTE]
-MELODY_LENGHT = 30
+            file.write(line)
+        file.close()
 
-for n in range(MELODY_LENGHT):
-    MELODY.append(get_next_note(CURR_NOTE))
+def summarize_transition_matrix(sample_file):
+    notes = convert_to_notes(get_midi_notes(sample_file))
 
-midi.generate(MELODY)
+    for i in range(len(notes) - 1):
+        source_note = notes[i]
+        target_note = notes[i + 1]
+
+        adj_list[source_note.upper()][target_note.upper()] += 1
+
+    return adj_list
+
+def laplace_smoothing(k=4):
+    for source_note in adj_list:
+        for target_note in adj_list[source_note]:
+            adj_list[source_note][target_note] += k
+
+    return adj_list
+
+def normalize():
+    for source_note in adj_list:
+        total_sum = sum([num for num in adj_list[source_note].values()])
+
+        for target_note in adj_list[source_note]:
+            adj_list[source_note][target_note] = adj_list[source_note][target_note] / total_sum
+
+    return adj_list
+
+def take_samples(source, target):
+    preload_transition_matrix(source)
+
+    base_dir = './samples'
+    for file in os.listdir(base_dir):
+        sample_path = os.path.join(base_dir, file)
+        summarize_transition_matrix(sample_path)
+
+    laplace_smoothing(k=4)
+    normalize()
+
+    save_transition_matrix(target)
